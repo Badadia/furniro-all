8037d3f3e84e26e34a6f08c777d8ef0f69f37805
gkgiann <lgian584@gmail.com>
2026-08-04 16:14:46 -0300

    refactor(PageBanner): remove unused logo prop and adjust image styles

--- frontend/src/components/Shop/PageBanner.tsx
+++ frontend/src/components/Shop/PageBanner.tsx
@@ -3,7 +3,6 @@ import BannerImage from "../../../public/Shop/shop-banner.png";
 
 type PageBannerProps = {
   topImage?: string;
-  logo?: string;
   title: string;
   breadcrumbHome: string;
   breadcrumbCurrent: string;
@@ -14,7 +13,6 @@ const PageBanner = ({
   title,
   className,
   topImage,
-  logo,
   breadcrumbHome,
   breadcrumbCurrent,
 }: PageBannerProps) => {
@@ -23,20 +21,10 @@ const PageBanner = ({
       <img
         src={topImage || BannerImage}
         alt=""
-        className="h-[116px] w-full object-cover sm:h-[216px] md:h-[316px]"
+        className="h-29 w-full object-cover sm:h-54 md:h-79"
       />
 
       <div className="absolute inset-x-0 bottom-6 flex flex-col items-center gap-0 sm:bottom-16 md:bottom-32">
-        {logo && (
-          <div className="relative h-[77px] w-[77px]">
-            <img
-              src={logo}
-              alt=""
-              className="absolute bottom-0 left-1/2 max-h-[43px] max-w-[43px] -translate-x-1/2 object-contain"
-            />
-          </div>
-        )}
-
         <h1 className="font-poppins text-5xl font-medium text-primary-text-200 sm:text-[36px] md:text-[42px]">
           {title}
         </h1>
