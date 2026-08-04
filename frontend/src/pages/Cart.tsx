93a902e0941b73dc6dbfcf8cf705b79cd2fb0e8c
gkgiann <lgian584@gmail.com>
2026-08-04 16:18:02 -0300

    feat: use PageBanner

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -2,16 +2,14 @@ import Benefits from "../components/Benefits/Benefits";
 import { CartHeaders } from "../components/Cart/CartHeaders";
 import { CartList } from "../components/Cart/CartList";
 import { CartTotals } from "../components/Cart/CartTotals";
+import PageBanner from "../components/Shop/PageBanner";
 
 export const Cart = () => {
   return (
     <div className="mb-2">
-      <div>
-        <h1>Cart (usar componente)</h1>
-        <span>{"Home > Cart"}</span>
-      </div>
+      <PageBanner breadcrumbCurrent="Cart" breadcrumbHome="Home" title="Cart" />
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-7 lg:gap-7 px-4 max-w-310 mx-auto mb-21.25">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-7 lg:gap-7 px-4 max-w-310 mx-auto mb-21.25 mt-18">
         <div className="col-span-2 overflow-x-auto md:overflow-x-visible">
           <CartHeaders />
           <CartList />
