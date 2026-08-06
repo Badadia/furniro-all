a08d2d39f75c2e8c045541d00acaf7c56d930674
jefferson-tenorio <tenoriojefferson@proton.me>
2026-08-06 02:46:23 -0300

    #69 fix: scroll to top on every page navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { RootLayout } from "./layout";
+import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
 import { Cart } from "./pages/Cart";
 import { Home } from "./pages/Home";
 import { Shop } from "./pages/Shop";
@@ -9,6 +10,7 @@ import { NotFound } from "./components/NotFound";
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="/" element={<Home />} />
