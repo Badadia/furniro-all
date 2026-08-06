c9989f5e1c595f0f350ecb3438f81d2466be8edf
gkgiann <lgian584@gmail.com>
2026-08-06 13:57:17 -0300

    fix: add empty cart message to CartList component

--- frontend/src/components/Cart/CartList.tsx
+++ frontend/src/components/Cart/CartList.tsx
@@ -3,6 +3,15 @@ import { CartItem } from "./CartItem";
 
 export function CartList() {
   const items = useCartStore((s) => s.items);
+  const isEmpty = useCartStore((s) => s.isEmpty());
+
+  if (isEmpty) {
+    return (
+      <div className="flex mt-16 items-center justify-center text-2xl font-medium">
+        Your cart is empty.
+      </div>
+    );
+  }
 
   return (
     <>
